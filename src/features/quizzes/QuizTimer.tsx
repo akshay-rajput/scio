0f1cf66e7f8e15e1078d7c3e55f9d0004e23ac9c
Akshay <akshayrajput102@gmail.com>
2021-07-23 22:12:43 +0530

    feat - quizSummary - handle edge case when no answer selected within time limit

--- src/features/quizzes/QuizTimer.tsx
+++ src/features/quizzes/QuizTimer.tsx
@@ -1,34 +1,42 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import {QuestionType} from './quizType';
 
 type QuizTimerProps = {
     // startTimer: boolean,
     updateQuestion: Function,
-    seconds: number
+    seconds: number,
+    currentQuestion: QuestionType
 }
-export const QuizTimer: React.FC<QuizTimerProps> = ({ seconds, updateQuestion }) => {
+export const QuizTimer: React.FC<QuizTimerProps> = ({ seconds, updateQuestion, currentQuestion }) => {
 
     const [timeLeft, setTimeLeft] = useState(seconds);
 
     useEffect(() => {
-      // exit early when we reach 0 and timer is still set to on
-      if (!timeLeft) {
-          updateQuestion();
-          return;
-      }
-  
-      const intervalId = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 1000);
-  
-      // clear interval on re-render to avoid memory leaks
-      return () => clearInterval(intervalId);
-      
-    }, [timeLeft, updateQuestion]);
+        // exit early when we reach 0 and timer is still set to on
+        if (!timeLeft) {
+
+            let questionInfo = {
+                question: currentQuestion,
+                selectedOption: 0,
+                points: 0
+            }
+            updateQuestion(questionInfo);
+            return;
+        }
+
+        const intervalId = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+
+        // clear interval on re-render to avoid memory leaks
+        return () => clearInterval(intervalId);
+
+    }, [timeLeft, updateQuestion, currentQuestion]);
 
     return (
         <>
             {
-                timeLeft < 10 ? `0${timeLeft}`: timeLeft
+                timeLeft < 10 ? `0${timeLeft}` : timeLeft
             }
         </>
     )
