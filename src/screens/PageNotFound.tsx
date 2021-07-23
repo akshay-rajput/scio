c9dbc7e5e80b420cdac5b207c0a2752b79668ae0
Akshay <akshayrajput102@gmail.com>
2021-07-23 15:52:56 +0530

    feat - add page not found

---
+++ src/screens/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageNotFound() {
+    return (
+        <div>
+            Oops the page you were looking for cannot be found.
+        </div>
+    )
+}
